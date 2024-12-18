d6f706cdc39683ed062b6494e24cb52bd5091ca9
mm0177 <mm0177@srmist.edu.in>
2024-12-18 12:27:48 +0530

    updated

--- setup.py
+++ setup.py
@@ -8,6 +8,22 @@ setup(
     version="0.0.1",
     author="meyyappan",
     author_email="",
-    install_requires=requirements,
+    install_requires=["pandas",
+"scikit-learn",
+"numpy",
+"seaborn". 
+"flask",
+"mlflow==2.2.2",
+"dvc",
+"ipykernel"
+"xgboost",
+
+
+
+"pytest==7.1.3",
+"tox==3.25.1",
+"black==22.8.0",
+"flake8==5.0.4",
+"mypy==0.971"],
     packages=find_packages(),
 )
